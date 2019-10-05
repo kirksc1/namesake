5747e11e11f34c4ba6d5be355c4f9397745614fa
Chris Kirk <kirksc1@gmail.com>
2019-10-05 15:40:04 -0500

    apply spring default naming after placeholder resolution

--- src/test/java/com/github/kirksc1/namesake/entity/FooRepository.java
+++ src/test/java/com/github/kirksc1/namesake/entity/FooRepository.java
@@ -3,6 +3,10 @@ package com.github.kirksc1.namesake.entity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface FooRepository extends JpaRepository<Foo, String> {
+public interface FooRepository extends JpaRepository<Foo, Long> {
+
+    Optional<Foo> findByName(String name);
 }
